165f86fd12b869ce48cab9b56f39edbebfe80065
Rite92 <riteclamp@gmail.com>
2024-12-23 15:39:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Smooth Scrolling Effect for Navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+});
